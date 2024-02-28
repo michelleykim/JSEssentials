852966c61b9a40141f7d3d6b18131e2ea9b202b9
michelleykim <yeojin011016@gmail.com>
2024-02-28 15:41:50 -0800

    update solution

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
+const items = [
+	{
+		Image: "assets/hoodie.svg",
+		Name: "Hoodie",
+		Type: "Clothing",
+		Quantity: 5,
+	},
+	{
+		Image: "assets/med kit.svg",
+		Name: "Med kit",
+		Type: "First aid",
+		Quantity: 2,
+	},
+	{
+		Image: "assets/seashells.svg",
+		Name: "Cute seashells",
+		Type: "Misc.",
+		Quantity: 11,
+	},
+	{ Image: "assets/cape.svg", Name: "Cape", Type: "Clothing", Quantity: 3 },
+];
+
 function addToken(row, col) {
 	console.log("row " + row + " col " + col + " cell is clicked!");
-
-	let cell = document.getElementById(row + "-" + col);
-	let boatToken = `<img src="assets/boat.svg" alt="ship-token" class="map-icon">`;
-	let treasureToken = `<img src="assets/treasure.svg" alt="ship-token" class="map-icon">`;
-	let XToken = `<img src="assets/X marks the spot.svg" alt="ship-token" class="map-icon">`;
-
-	if (cell.innerHTML == "&nbsp;") {
-		cell.innerHTML = boatToken;
-	} else if (cell.innerHTML == boatToken) {
-		cell.innerHTML = treasureToken;
-	} else if (cell.innerHTML == treasureToken) {
-		cell.innerHTML = XToken;
-	} else if (cell.innerHTML == XToken) {
-		cell.innerHTML = "&nbsp;";
-	}
 }
 
 function filterType(type) {
-	let items = [
-		{
-			Image: "assets/hoodie.svg",
-			Name: "Hoodie",
-			Type: "Clothing",
-			Quantity: 5,
-		},
-		{
-			Image: "assets/med kit.svg",
-			Name: "Med kit",
-			Type: "First aid",
-			Quantity: 2,
-		},
-		{
-			Image: "assets/seashells.svg",
-			Name: "Cute seashells",
-			Type: "Misc.",
-			Quantity: 11,
-		},
-		{ Image: "assets/cape.svg", Name: "Cape", Type: "Clothing", Quantity: 3 },
-	];
+	// This function is missing something!
+	// Find what's missing and make the filter work :))
+
+	console.log("Filtering items: " + type);
+
+	let filteredItems = items;
 
 	if (type != "") {
-		items = items.filter((item) => item.Type == type);
+		filteredItems = items.filter((item) => item.Type == type);
 	}
 
+	makeTable(filteredItems);
+}
+
+function sortInventory() {
+	// This function is missing something!
+	// Find what's missing and make the sorting work :))
+
+	console.log("Sorting items by Quantity");
+
+	let sortedItems = items;
+
+	sortedItems.sort((a, b) => a.Quantity - b.Quantity);
+
+	makeTable(sortedItems);
+}
+
+function makeTable(array) {
 	let table = document.getElementById("inventory-table");
 
 	table.innerHTML = `<tr>
@@ -53,7 +62,7 @@ function filterType(type) {
   <th scope="col">Quantity</th>
 </tr>`;
 
-	for (let item of items) {
+	for (let item of array) {
 		const newRow = document.createElement("tr");
 
 		const tdImage = document.createElement("td");
